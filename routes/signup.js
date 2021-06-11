548b54eef29bc22beede87bfb1d56c312314736d
Guillaume <Gui.lefort.17@gmail.com>
2021-06-11 14:33:58 +0200

    init bcrypt

--- routes/signup.js
+++ routes/signup.js
@@ -1,9 +1,9 @@
 /* eslint-disable camelcase */
 /* eslint-disable func-names */
 const express = require('express');
-
 const router = express.Router();
 const pool = require('../config/mysql');
+const bcrypt = require('bcrypt');
 
 router.get('/', (request, response) => {
   console.log(request);
@@ -19,39 +19,39 @@ router.get('/', (request, response) => {
 router.post('/', (request, response) => {
   // TODO : Valeurs de tests, a supprimer plus tard.
   const role_id = '1';
-  const birthdate = '1983-02-05 00:00:00';
   // TODO : Valeurs de tests, a supprimer plus tard.
   const { formContent } = request.body;
-  pool.query(
-    'INSERT INTO user (firstname, lastname,birthdate, email, password, RPPS, SIRET, address, phone, country, website,role_id) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)',
-    [
-      formContent.firstname,
-      formContent.lastname,
-      // formContent.picture,
-      // formContent.birthdate,
-      birthdate,
-      formContent.email,
-      formContent.password,
-      formContent.RPPS,
-      formContent.SIRET,
-      formContent.address,
-      formContent.phone,
-      formContent.country,
-      formContent.website,
-      role_id,
-    ],
-    (error) => {
-      if (error) {
-        response.status(500).send('Error Creating new User');
-        console.log(formContent);
-        console.log(error);
-      } else {
-        response.status(200).send('User created');
-        console.log(request.body);
-        // console.log(result);
-      }
+  //cryptage du mot de passe
+  bcrypt.hash(formContent.password, 10, (error, hash) => {
+    if (error) {
+      console.log(`bcrypt ${error}`);
+    } else {
+      pool.query(
+        'INSERT INTO user (firstname, lastname, birthdate, email, password, RPPS, SIRET, address, phone, country, website, role_id) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)',
+        [
+          formContent.firstname,
+          formContent.lastname,
+          // formContent.picture,
+          formContent.birthdate,
+          formContent.email,
+          hash,
+          formContent.RPPS,
+          formContent.SIRET,
+          formContent.address,
+          formContent.phone,
+          formContent.country,
+          formContent.website,
+          role_id,
+        ],
+        (error) => {
+          if (error) {
+            response.status(500).send(`Error Creating new User${error}`);
+          } else {
+            response.status(200).send('User created');
+          }
+        }
+      );
     }
-  );
+  });
 });
-
 module.exports = router;
