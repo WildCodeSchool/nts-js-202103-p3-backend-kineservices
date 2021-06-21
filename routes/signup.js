80b0ae7f5c5ab28c0d478747f4e65e7718462fe0
SirHarveyBix <79848679+SirHarveyBix@users.noreply.github.com>
2021-06-21 13:58:43 +0200

    pull update

--- routes/signup.js
+++ routes/signup.js
@@ -1,12 +1,12 @@
 /* eslint-disable camelcase */
 /* eslint-disable func-names */
 const express = require('express');
+
 const router = express.Router();
-const pool = require('../config/mysql');
 const bcrypt = require('bcrypt');
+const pool = require('../config/mysql');
 
 router.get('/', (request, response) => {
-  console.log(request);
   pool.query('SELECT * FROM user', (error, results) => {
     if (error) {
       response.status(500).send(error);
@@ -19,15 +19,14 @@ router.get('/', (request, response) => {
 router.post('/', (request, response) => {
   // TODO : Valeurs de tests, a supprimer plus tard.
   const role_id = '1';
-  // TODO : Valeurs de tests, a supprimer plus tard.
+
   const { formContent } = request.body;
-  //cryptage du mot de passe
   bcrypt.hash(formContent.password, 10, (error, hash) => {
     if (error) {
       console.log(`bcrypt ${error}`);
     } else {
       pool.query(
-        'INSERT INTO user (firstname, lastname, birthdate, email, password, RPPS, SIRET, address, phone, country, website, role_id) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)',
+        'INSERT INTO user (firstname, lastname, birthdate, email, password, RPPS, siret, address, phone, country, website, role_id) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)',
         [
           formContent.firstname,
           formContent.lastname,
@@ -36,13 +35,14 @@ router.post('/', (request, response) => {
           formContent.email,
           hash,
           formContent.RPPS,
-          formContent.SIRET,
+          formContent.siret,
           formContent.address,
           formContent.phone,
           formContent.country,
           formContent.website,
           role_id,
         ],
+        // eslint-disable-next-line no-shadow
         (error) => {
           if (error) {
             response.status(500).send(`Error Creating new User${error}`);
