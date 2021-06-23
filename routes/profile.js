629089aa56a6aea2e09306a39daba73c0afe744e
Guillaume <Gui.lefort.17@gmail.com>
2021-06-23 11:37:39 +0200

    envoies de la validation de connection + id vers le front (a utiliser pour profile et header une fois connecté)

---
+++ routes/profile.js
@@ -0,0 +1,14 @@
+const express = require('express');
+
+const router = express.Router();
+const pool = require('../config/mysql');
+
+router.get('/', function (request, response) {
+  pool.query('SELECT * FROM documentation', (error, results) => {
+    if (error) {
+      response.status(500).send(error);
+    } else {
+      response.send(results);
+    }
+  });
+});
--- routes/signIn.js
+++ routes/signIn.js
@@ -7,6 +7,7 @@ const pool = require('../config/mysql');
 
 router.post('/', (request, response) => {
   const { email, password } = request.body;
+  const isConnected = [];
   pool.query(
     'SELECT * FROM user WHERE email = ?',
     [email],
@@ -14,18 +15,25 @@ router.post('/', (request, response) => {
       if (error) {
         response.send(error);
       } else {
-        bcrypt.compare(password, results[0].password, (error, res) => {
-          // compare le mot de passe de la db,
-          if (res) {
-            response.send(res);
-            //  renvoies true
-          } else if (error) {
-            response.send(error);
-          } else {
-            response.send(res);
-            // renvoies false
+        // response.status(200).send(results[0].id);
+        bcrypt.compare(
+          password,
+          results[0].password,
+          (error, responseCrypted) => {
+            // compare le mot de passe de la db,
+            if (responseCrypted) {
+              isConnected.push(responseCrypted, results[0].id);
+              response.status(200).send(isConnected);
+              //  renvoies true + id
+            } else if (error) {
+              response.send(error);
+            } else {
+              response.status(403).send('Votre mot de passe est eronné');
+              // renvoies false
+            }
+            // response.status(200).send(res, results[0].id);
           }
-        });
+        );
       }
     }
   );
