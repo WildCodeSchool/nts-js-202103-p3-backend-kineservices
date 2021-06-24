8abbc17a9ad5abc70b43b6ef31c0a9ff442da6ef
Jonathan Rousseau <jonathan.rousseau.wcs@gmail.com>
2021-06-24 11:54:29 +0200

    creation des methodes get post put delete sur formation

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const documentationRouter = require('./routes/documentation');
 const signupRouter = require('./routes/signup');
 const signIn = require('./routes/signIn');
 const categoryRouter = require('./routes/category');
+const formationRouter = require('./routes/formation');
 
 const app = express();
 app.use(express.json());
@@ -21,6 +22,7 @@ app.use('/documentation', documentationRouter);
 app.use('/connexion', signIn);
 app.use('/signup', signupRouter);
 app.use('/category', categoryRouter);
+app.use('/formation', formationRouter);
 
 app.listen(port, () => {
   console.log(`Express server listening on ${port}`);
