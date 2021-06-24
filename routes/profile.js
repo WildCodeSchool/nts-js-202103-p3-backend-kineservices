3681143dae0ea486d9679b7da91384a0e800bb9d
Guillaume <Gui.lefort.17@gmail.com>
2021-06-24 09:50:18 +0200

    recuperation info user

--- routes/profile.js
+++ routes/profile.js
@@ -5,11 +5,8 @@ const pool = require('../config/mysql');
 
 router.post('/:id', function (request, response) {
   const { id } = request.params;
-
-  console.log(id);
   pool.query('SELECT * FROM user WHERE id = ? ', [id], (error, results) => {
     if (error) {
-      console.log(error);
       response.status(500).send(error);
     } else {
       response.send(results);
