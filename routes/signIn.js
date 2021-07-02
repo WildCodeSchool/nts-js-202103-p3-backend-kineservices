e66292a151b68bc469bbab94aecd696127dd8cf6
Ninon Maraval <maravalninon@gmail.com>
2021-07-02 16:45:20 +0200

    ajout accolade manquante

--- routes/signIn.js
+++ routes/signIn.js
@@ -18,52 +18,61 @@ router.post('/', authJwt, (request, response) => {
       if (error) {
         response.status(500).send(error);
       } else {
-        bcrypt.compare(password, results[0].password, (error, res) => {
-          if (error) {
-            response.status(500).send(error);
-          } else {
-            response.send(res);
+        bcrypt.compare(
+          password,
+          results[0].password,
+          (errorBCrypt, resultsBCrypt) => {
+            if (errorBCrypt) {
+              response.status(500).send(errorBCrypt);
+            } else {
+              response.send(resultsBCrypt);
+            }
           }
-        });
+        );
 
-  if (!email || !password) {
-    response
-      .status(400)
-      .send('Veuillez sasir votre mot de passe ou votre email');
-  } else {
-    pool.query(
-      'SELECT * FROM user WHERE email = ?',
-      [email],
-      (error, results) => {
-        if (error) {
-          response.status(500).send(error);
-        } else if (results.length === 0) {
-          response.status(403).send(`invalid email`);
+        if (!email || !password) {
+          response
+            .status(400)
+            .send('Veuillez sasir votre mot de passe ou votre email');
         } else {
-          bcrypt.compare(
-            password,
-            results[0].password,
-            (err, responseCrypted) => {
-              if (responseCrypted) {
-                const user = {
-                  id: results[0].id,
-                  email: results[0].email,
-                };
-                const token = jwt.sign({ id: user.id }, JWT_AUTH_SECRET, {
-                  expiresIn: 10000,
-                });
-                response.status(200).send({ user, token });
-              } else if (err) {
-                response.send(err);
+          pool.query(
+            'SELECT * FROM user WHERE email = ?',
+            [email],
+            (err, res) => {
+              if (error) {
+                response.status(500).send(err);
+              } else if (res.length === 0) {
+                response.status(403).send(`invalid email`);
               } else {
-                response.status(403).send('Votre mot de passe est eronné');
+                bcrypt.compare(
+                  password,
+                  results[0].password,
+                  (errCrypted, responseCrypted) => {
+                    if (responseCrypted) {
+                      const user = {
+                        id: results[0].id,
+                        email: results[0].email,
+                      };
+                      const token = jwt.sign({ id: user.id }, JWT_AUTH_SECRET, {
+                        expiresIn: 10000,
+                      });
+                      response.status(200).send({ user, token });
+                    } else if (errCrypted) {
+                      response.send(errCrypted);
+                    } else {
+                      response
+                        .status(403)
+                        .send('Votre mot de passe est eronnÃ©');
+                    }
+                  }
+                );
               }
             }
           );
         }
       }
-    );
-  }
+    }
+  );
 });
 
 module.exports = router;
