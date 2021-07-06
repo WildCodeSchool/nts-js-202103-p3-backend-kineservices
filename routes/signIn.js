80c4b430b0cfe109e38388647b406dee88af8892
Guillaume <Gui.lefort.17@gmail.com>
2021-07-06 13:55:23 +0200

    resoltion des problemes liés a la connection, init de fs>upload files

--- routes/signIn.js
+++ routes/signIn.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-shadow */
 const express = require('express');
 
 const router = express.Router();
@@ -11,65 +10,44 @@ const { JWT_AUTH_SECRET } = process.env;
 
 router.post('/', authJwt, (request, response) => {
   const { email, password } = request.body;
-
-  pool.query(
-    'SELECT * FROM user WHERE email = ?',
-    [email],
-    (error, results) => {
-      if (error) {
-        response.status(500).send(error);
-      } else {
-        bcrypt.compare(password, results[0].password, (error, res) => {
-          if (error) {
-            response.status(500).send(error);
-          } else {
-            response.send(res);
-          }
-        });
-
-        if (!email || !password) {
-          response
-            .status(400)
-            .send('Veuillez sasir votre mot de passe ou votre email');
+  if (!email || !password) {
+    response
+      .status(400)
+      .send('Veuillez sasir votre mot de passe ou votre email');
+  } else {
+    pool.query(
+      'SELECT * FROM user WHERE email = ?',
+      [email],
+      (error, results) => {
+        if (error) {
+          response.status(500).send(error);
+        } else if (results.length === 0) {
+          response.status(403).send(`invalid email`);
         } else {
-          pool.query(
-            'SELECT * FROM user WHERE email = ?',
-            [email],
-            (error, results) => {
-              if (error) {
-                response.status(500).send(error);
-              } else if (results.length === 0) {
-                response.status(403).send(`invalid email`);
+          bcrypt.compare(
+            password,
+            results[0].password,
+            (errorCrypted, responseCrypted) => {
+              if (responseCrypted) {
+                const user = {
+                  id: results[0].id,
+                  email: results[0].email,
+                };
+                const token = jwt.sign({ id: user.id }, JWT_AUTH_SECRET, {
+                  expiresIn: 10000,
+                });
+                response.status(200).send({ user, token });
+              } else if (errorCrypted) {
+                response.send(errorCrypted);
               } else {
-                bcrypt.compare(
-                  password,
-                  results[0].password,
-                  (errorCrypted, responseCrypted) => {
-                    if (responseCrypted) {
-                      const user = {
-                        id: results[0].id,
-                        email: results[0].email,
-                      };
-                      const token = jwt.sign({ id: user.id }, JWT_AUTH_SECRET, {
-                        expiresIn: 10000,
-                      });
-                      response.status(200).send({ token, user });
-                    } else if (errorCrypted) {
-                      response.send(errorCrypted);
-                    } else {
-                      response
-                        .status(403)
-                        .send('Votre mot de passe est eronné');
-                    }
-                  }
-                );
+                response.status(403).send('Votre mot de passe est eronné');
               }
             }
           );
         }
       }
-    }
-  );
+    );
+  }
 });
 
 module.exports = router;
