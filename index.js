a25ef9ebe47a611c584f27cce9dbc5d654631c06
bastienwcs <bastien.krettly@wildcodeschool.com>
2021-07-16 11:59:41 +0200

    fixes

--- index.js
+++ index.js
@@ -21,12 +21,6 @@ app.use(
 );
 app.use(express.static('public'));
 
-app.use(express.static('public'));
-
-app.get('/', (req, res) => {
-  res.send('Hello World');
-});
-
 app.use('/documentation', documentationRouter);
 app.use('/category', categoryRouter);
 app.use('/service', serviceRouter);
